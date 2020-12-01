5dfbd9d1186e6e81b34282fced3112391f2fd6de
John Pavek <JohnQPavek@gmail.com>
2020-11-30 21:24:59 -0600

    Refining text positions

--- src/templator.js
+++ src/templator.js
@@ -10,20 +10,20 @@ img.onload = function (e) {
     canvas.width = img.width;
     canvas.height = img.height;
     ctx.drawImage(img, 0, 0);
-    ctx.font= "26px cursive"
+    ctx.font = "30px cursive"
 
-    var text = prompt("Word Count Today?");
-    ctx.fillText(text, 475, 270);
+    var textX = 475;
 
-    var text = prompt("Word Count Total?");
-    ctx.fillText(text, 475, 330);
+    var text = prompt("Word Count Today?").trim();
+    ctx.fillText(text, textX, 275);
 
-    
-    var text = prompt("Total Goal?");
-    ctx.fillText(text, 475, 390);
+    var text = prompt("Word Count Total?").trim();
+    ctx.fillText(text, textX, 335);
 
-    
-    var text = prompt("Date?");
-    ctx.fillText(text, 475, 450);
+    var text = prompt("Total Goal?").trim();
+    ctx.fillText(text, textX, 395);
+
+    var text = prompt("Date?").trim();
+    ctx.fillText(text, textX, 455);
 }
 img.src = image;
